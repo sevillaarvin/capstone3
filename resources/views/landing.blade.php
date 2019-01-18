099fdbed4b308fe1a13f388680c6e440bbe08787
Arvin Sevilla <sevillaarvin@gmail.com>
2019-01-18 10:19:07 +0800

    Implement sliding side-bar
    Refactor post submission.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/slideout.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
           integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <title>Yeet</title>
@@ -13,17 +14,26 @@
 <body>
 <style>
 </style>
-<nav class="side-nav side-nav--closed">
-    <div class="side-nav__overlay">
-        &nbsp;
-    </div>
-    <ul class="side-nav__content nav">
+<nav id="menu" class="side-nav side-nav--closed">
+    <ul class="side-nav__content nav flex-column text-center p-5">
         <li class="nav-item">
-            <a href="#" class="nav-link">Test</a>
+            <img src="http://i.pravatar.cc/300" alt="Avatar" class="side-nav__avatar">
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Profile</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Yeets</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Greets</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Meets</a>
         </li>
     </ul>
 </nav>
-<main>
+<main id="panel" class="panel">
     <div class="container">
         <div class="row">
             <div class="col">
@@ -33,7 +43,7 @@
                     <div class="lead">Repeat!</div>
                 </div>
                 <nav class="header__navbar navbar navbar-expand-lg navbar-light bg-light">
-                    <span class="mr-auto">MENU TOGGLE</span>
+                    <span class="mr-auto toggle-button">MENU TOGGLE</span>
                     <span class="ml-auto">
                         AVATAR
                         <a href="{{ route('logout') }}"
@@ -51,16 +61,19 @@
                     @guest
                         <form method="POST" action="{{ route("login") }}">
                             {{ csrf_field() }}
-                            <div class="login__username">
-                                I beat like <input id="username" name="username" type="text">
+                            <div class="login__username form-group">
+                                I beat like <input id="username" name="username" type="text"
+                                                   class="login__username-input custom-control-inline"
+                                                   placeholder="username">
                                 @if ($errors->has('username'))
                                     <div class="help-block">
                                         <strong>{{ $errors->first('username') }}</strong>
                                     </div>
                                 @endif
                             </div>
-                            <div class="login__password">
-                                Poteat <input id="password" name="password" type="password">!
+                            <div class="login__password form-group">
+                                Poteat <input id="password" name="password" type="password"
+                                              class="login__password-input" placeholder="password">!
                                 @if ($errors->has('password'))
                                     <div class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -76,27 +89,30 @@
         @auth
             <div class="row">
                 <div class="col">
-                    <div class="post__form">
-                        <textarea id="post" cols="30" rows="5" placeholder="Yeet!!!"
+                    <form id="post-form" name="post-form" class="post__form">
+                        <textarea id="post-content" name="content" cols="30" rows="5" placeholder="Yeet!!!"
                                   class="post__content form-control mb-1"></textarea>
-                        <button id="post-submit" class="post__submit btn btn-primary form-control" type="button">
+                        <button id="post-submit" class="post__submit btn btn-primary form-control" type="submit">
                             Yeet!
                         </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         @endauth
         <div class="row">
             @foreach($posts as $post)
                 <div class="col-12 my-3">
-                        <div>
-                            <div>
-                                {{ $post->created_at->diffForHumans() }}
-                            </div>
-                            <div>
-                                {{ $post->content }}
-                            </div>
+                    <div class="row">
+                        <div class="col-3">
+                            {{ 'AVATAR' }}
                         </div>
+                        <div class="col-6">
+                            {{ $post->content }}
+                        </div>
+                        <div class="col-3">
+                            {{ $post->created_at->diffForHumans() }}
+                        </div>
+                    </div>
                 </div>
             @endforeach
         </div>
@@ -112,26 +128,54 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
         integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
         crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.min.js"></script>
+<script>
+    var slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70
+    });
+
+    // Toggle button
+    document.querySelector('.toggle-button').addEventListener('click', function () {
+        slideout.toggle();
+    });
+
+    function close(eve) {
+        eve.preventDefault();
+        slideout.close();
+    }
+
+    slideout
+        .on('beforeopen', function () {
+            this.panel.classList.add('panel-open');
+        })
+        .on('open', function () {
+            this.panel.addEventListener('click', close);
+        })
+        .on('beforeclose', function () {
+            this.panel.classList.remove('panel-open');
+            this.panel.removeEventListener('click', close);
+        });
+</script>
+
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script>
             @auth
-    const post = document.querySelector("#post")
-    post.addEventListener("keydown", (e) => {
-        const key = e.which || e.keyCode || 0
-        if (key === 13) {
-            createPost(post, post.value)
-                .then(() => post.value = "")
-                .catch(handleErrors)
-        }
-    })
-    const postSubmit = document.querySelector("#post-submit")
-    postSubmit.addEventListener("click", (e) => {
-        createPost(post, post.value)
-            .then(() => post.value = "")
+    const postForm = document.querySelector("#post-form")
+    postForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const postContent = e.target["post-content"]
+        const content = postContent.value
+        createPost(content)
+            .then(() => postContent.value = "")
             .catch(handleErrors)
     })
 
-    const createPost = async (post, content) => {
+    const createPost = async (content) => {
         try {
             await axios.post("{{ route("yeet.store") }}", {
                 content,
@@ -147,7 +191,7 @@
             return
         }
 
-        const { status } = e.response
+        const {status} = e.response
         if (status == 422) {
             // TODO: Show error message, yeet cannot be empty
         }
